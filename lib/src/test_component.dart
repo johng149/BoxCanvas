6a53645064697b0ae97ae1ff74269a877b61732b
johng149 <johnguo149@gmail.com>
2022-08-24 19:37:05 -0400

    Add example project

---
+++ lib/src/test_component.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TestComponent extends StatelessWidget {
+  const TestComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Test!");
+  }
+}
