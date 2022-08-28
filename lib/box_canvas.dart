b426beff21944b8a733b58b5ef219f6d8a09180b
johng149 <93888236+johng149@users.noreply.github.com>
2022-08-28 16:42:20 -0400

    Added experimental stub (#1)
    * Add example project
    
    * Added project to example dependency
    
    * Add main widget stub

--- lib/box_canvas.dart
+++ lib/box_canvas.dart
@@ -1,7 +1,3 @@
 library box_canvas;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/test_component.dart' show TestComponent;
