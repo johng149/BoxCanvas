c4bf6407c434eb104cf27c881b275e98081917db
johng149 <johnguo149@gmail.com>
2022-08-28 17:09:30 -0400

    Add canvas draggable stub

---
+++ lib/src/components/canvas_draggable.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CanvasDraggable extends StatefulWidget {
+  const CanvasDraggable({super.key});
+
+  @override
+  State<CanvasDraggable> createState() => _CanvasDraggableState();
+}
+
+class _CanvasDraggableState extends State<CanvasDraggable> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
