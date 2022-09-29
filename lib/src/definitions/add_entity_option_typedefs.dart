b384efed5d20fed20183231d82f86d1a32f2e41c
johng149 <johnguo149@gmail.com>
2022-09-29 19:49:41 -0400

    Add comment about add entity function

--- lib/src/definitions/add_entity_option_typedefs.dart
+++ lib/src/definitions/add_entity_option_typedefs.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-///Function called when adding entity data (presumably to given providers)
+///Giving [context] and [id], create [Widget] that will appear on canvas
+///
+///It is not the responsibility of this function to add the [Widget] it creates
+///to the canvas since the only thing it does is produce [Widget] to be added
 typedef AddEntityFunction = Widget Function(
     {required BuildContext context, required String id});
 
