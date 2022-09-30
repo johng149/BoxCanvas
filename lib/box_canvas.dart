f511daa514f4019764bf6e215f696b083ddabc70
johng149 <johnguo149@gmail.com>
2022-09-29 20:08:26 -0400

    Exporting library parts so others can use it

--- lib/box_canvas.dart
+++ lib/box_canvas.dart
@@ -1,3 +1,17 @@
 library box_canvas;
 
 //export 'src/test_component.dart' show TestComponent;
+
+//exports for [AddEntityOption] class, used when constructing Canvas component
+export 'src/abstract_providers/add_entity_option.dart' show AddEntityOption;
+
+//exports for providers used when constructing Canvas component
+export 'src/abstract_providers/entity_body_provider_interface.dart'
+    show IEntityBodyNotifier;
+export 'src/abstract_providers/entity_position_provider_interface.dart'
+    show IEntityPositionNotifier;
+export 'src/abstract_providers/global_offset_provider_interface.dart'
+    show IGlobalOffsetNotifier;
+
+//BoxCanvas export
+export 'src/components/box_canvas.dart' show BoxCanvas;
